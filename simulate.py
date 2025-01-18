98e8b7becf70d3ddbd6ee042e74770420dac65ac
megweyer <mweyer@uvm.edu>
2025-01-18 16:41:27 -0500

    adding the steps into the physics world

--- simulate.py
+++ simulate.py
@@ -1,4 +1,11 @@
 import pybullet as p
+
+#import physics engine and
 pysicsClient = p.connect(p.GUI)
+
+#steps inside the physics world
+for i in range (1000):
+    p.stepSimulation()
+
 p.disconnect()
 
