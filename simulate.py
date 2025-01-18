a4e8faeff06edd1940cacb441f5ecd9a42cfcc1e
megweyer <mweyer@uvm.edu>
2025-01-18 16:48:27 -0500

    slowing down the simulation world

--- simulate.py
+++ simulate.py
@@ -1,4 +1,5 @@
 import pybullet as p
+import time as t
 
 #import physics engine and
 pysicsClient = p.connect(p.GUI)
@@ -6,6 +7,9 @@ pysicsClient = p.connect(p.GUI)
 #steps inside the physics world
 for i in range (1000):
     p.stepSimulation()
+    #call times sleep function to slow down the simulation
+    t.sleep(1. / 240)
+    print (i) #tell us how long each iteration takes
 
 p.disconnect()
 
