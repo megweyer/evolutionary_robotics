c69a12b6224a8e166e327c830aaf3f336821745b
megweyer <mweyer@uvm.edu>
2025-01-24 18:20:37 -0500

    add floor to world

--- simulate.py
+++ simulate.py
@@ -1,11 +1,16 @@
 import pybullet as p
+import pybullet_data
 import time as t
 
 #import physics engine and
 pysicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-#add forces into world
+#add gravity into world
 p.setGravity(0,0,-9.8)
+
+#add a floor so the box doesn't fall indefinitely
+planeId = p.loadURDF("plane.urdf")
 
 #allow box to show in simulation
 p.loadSDF("box.sdf")
