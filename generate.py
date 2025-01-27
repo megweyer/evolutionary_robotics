cc529ab454dbb53e340f809df652b3ecb5ac85e0
megweyer <mweyer@uvm.edu>
2025-01-27 11:00:01 -0500

    updates to many links for part D

--- generate.py
+++ generate.py
@@ -1,10 +1,31 @@
 import pyrosim.pyrosim as pyrosim
 
 #where information about my world will be stored
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-#stores box
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+#identify variables
+rows = 5 #number of rows in grid
+columns = 5 #number of columns in grid
+tower_height = 10 #how tall the tower is gonna be, how many blocks high
+start_size = 1.0  # base block size
+
+# Loop to create 10 blocks in the tower
+for row in range(rows):
+    for column in range (columns):
+        #find position for this tower
+        x = row
+        y = column
+        z = start_size / 2
+
+        #generate size at (x,y)
+        block_size = start_size
+        for level in range (tower_height):
+            # Send the cube with the current position and size
+            pyrosim.Send_Cube(name = f"Box_{row,column, level}", pos = [x,y,z], size = [block_size, block_size, block_size])
+
+            # Update the size for the next block (reduce by 10% in each dimension)
+            z += block_size
+            block_size += 0.9 #make each cube 90% of the previous
 
 #close the sdf file
 pyrosim.End()
