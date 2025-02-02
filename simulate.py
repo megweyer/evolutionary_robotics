b9f85e3bab14b761a442126209f55c661e1af058
megweyer <mweyer@uvm.edu>
2025-02-01 19:28:36 -0500

    called the functions in simulate

--- simulate.py
+++ simulate.py
@@ -1,6 +1,15 @@
 import pybullet as p
 import pybullet_data
 import time as t
+from generate import Create_world as CW
+from generate import Create_robot as CR
+from generate import Create_link_practice as CLP
+
+#call create world
+#CW()
+#CLP ()
+# call robot function
+CR()
 
 #import physics engine and
 pysicsClient = p.connect(p.GUI)
@@ -12,8 +21,11 @@ p.setGravity(0,0,-9.8)
 #add a floor so the box doesn't fall indefinitely
 planeId = p.loadURDF("plane.urdf")
 
+#add a floor so the box doesn't fall indefinitely
+robotId = p.loadURDF("body.urdf")
+
 #allow box to show in simulation
-p.loadSDF("boxes.sdf")
+#p.loadSDF("world.sdf")
 
 #steps inside the physics world
 for i in range (1000):
@@ -21,6 +33,7 @@ for i in range (1000):
     #call times sleep function to slow down the simulation
     t.sleep(1. / 100)
     print (i) #tell us how long each iteration takes
+
 
 p.disconnect()
 
