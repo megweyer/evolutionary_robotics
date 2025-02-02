b87073e0b8f1f2a66ce5d3d561e4d606c5ddb1c8
megweyer <mweyer@uvm.edu>
2025-02-01 19:27:03 -0500

    completed part E, joints. MAde a robot with a torso and two legs

--- generate.py
+++ generate.py
@@ -1,32 +1,64 @@
 import pyrosim.pyrosim as pyrosim
 
-#where information about my world will be stored
-pyrosim.Start_SDF("boxes.sdf")
 
-#identify variables
-rows = 5 #number of rows in grid
-columns = 5 #number of columns in grid
-tower_height = 10 #how tall the tower is gonna be, how many blocks high
-start_size = 1.0  # base block size
+#create a function called create_world
+def Create_world ():
+    # where information about my world will be stored
+    pyrosim.Start_SDF("world.sdf")
 
-# Loop to create 10 blocks in the tower
-for row in range(rows):
-    for column in range (columns):
-        #find position for this tower
-        x = row
-        y = column
-        z = start_size / 2
+    # Send the cube with the current position and size
+    pyrosim.Send_Cube(name=f"Box_1", pos=[1, 1, 1], size=[1, 1, 1])
 
-        #generate size at (x,y)
-        block_size = start_size
-        for level in range (tower_height):
-            # Send the cube with the current position and size
-            pyrosim.Send_Cube(name = f"Box_{row,column, level}", pos = [x,y,z], size = [block_size, block_size, block_size])
+    # close the sdf file
+    pyrosim.End()
 
-            # Update the size for the next block (reduce by 10% in each dimension)
-            z += block_size
-            block_size += 0.9 #make each cube 90% of the previous
+#create function for robot
+def Create_link_practice ():
+    pyrosim.Start_URDF("body.urdf")
 
-#close the sdf file
-pyrosim.End()
+    #create practice for joints and links
+    pyrosim.Send_Cube(name= "link0", pos=[0,0, 0.5], size=[1, 1, 1])
+    #create joint
+    pyrosim.Send_Joint(name = "link0_link1", parent = "link0", child = "link1", type = "revolute", position = [0, 0, 1.0])
+    #create leg - position is relative to joint 1, that's why it is 0,0,0.5 again
+    #everything from here on out will be relative to the previous joint
+    pyrosim.Send_Cube (name = "link1", pos = [0.0,0,0.5], size = [1,1,1])
+    #create the next joint
+    pyrosim.Send_Joint(name="link1_link2", parent="link1", child="link2", type="revolute", position=[0, 0, 1.0])
+    #create next link
+    pyrosim.Send_Cube(name="link2", pos=[0.0, 0, 0.5], size=[1, 1, 1])
+    #create new joint
+    pyrosim.Send_Joint(name="link2_link3", parent="link2", child="link3", type="revolute", position=[0, 0.5, 0.5])
+    #create new link
+    pyrosim.Send_Cube(name="link3", pos = [0,0.5,0], size = [1,1,1])
+    #create new joint
+    pyrosim.Send_Joint(name="link3_link4", parent="link3", child="link4", type="revolute", position=[0, 1, 0])
+    #create new link
+    pyrosim.Send_Cube(name="link4", pos = [0,0.5,0], size = [1,1,1])
+    #create new joint
+    pyrosim.Send_Joint(name="link4_link5", parent="link4", child="link5", type="revolute", position=[0, 0.5, -0.5])
+    #create new link
+    pyrosim.Send_Cube(name="link5", pos = [0,0,-0.5], size = [1,1,1])
+    #create new joint
+    pyrosim.Send_Joint(name="link5_link6", parent="link5", child="link6", type="revolute", position=[0, 0, -1])
+    #create new link
+    pyrosim.Send_Cube(name="link6", pos = [0,0,-0.5], size = [1,1,1])
+    pyrosim.End()
+
+#create new robot with joints
+def Create_robot ():
+    pyrosim.Start_URDF("body.urdf")
+    #create torso
+    pyrosim.Send_Cube(name="torso", pos=[0.0, 1, 1.5], size=[1, 1, 1])
+    # create joint
+    pyrosim.Send_Joint(name="torso_back", parent="torso", child="back", type="revolute", position=[0, 0.5, 1.0])
+    #create back leg
+    pyrosim.Send_Cube(name= "back", pos=[0,-0.5, -0.5], size=[1, 1, 1])
+    #create joint
+    pyrosim.Send_Joint(name = "torso_front", parent = "torso", child = "front", type = "revolute", position = [0,1,0])
+    #create leg
+    pyrosim.Send_Cube(name = "front", pos = [0,1,0.5], size = [1,1,1])
+
+    pyrosim.End()
+
 
