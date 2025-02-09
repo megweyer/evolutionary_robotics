a70895b9d99abcfa2f9c48a46747ae260378961f
megweyer <mweyer@uvm.edu>
2025-02-08 20:40:46 -0500

    end of part F - sensors

--- simulate.py
+++ simulate.py
@@ -1,39 +1,59 @@
+import os
 import pybullet as p
 import pybullet_data
 import time as t
+import numpy
+import pyrosim.pyrosim as pyrosim
 from generate import Create_world as CW
 from generate import Create_robot as CR
-from generate import Create_link_practice as CLP
 
-#call create world
-#CW()
-#CLP ()
-# call robot function
+# Call create world
+CW()
+
+# Call robot function
 CR()
 
-#import physics engine and
+# Import physics engine and
 pysicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-#add gravity into world
+# Add gravity into world
 p.setGravity(0,0,-9.8)
 
-#add a floor so the box doesn't fall indefinitely
+# Add a floor so the box doesn't fall indefinitely
 planeId = p.loadURDF("plane.urdf")
 
-#add a floor so the box doesn't fall indefinitely
+# Add robot
 robotId = p.loadURDF("body.urdf")
 
-#allow box to show in simulation
-#p.loadSDF("world.sdf")
+# Prepare simulation
+pyrosim.Prepare_To_Simulate(robotId)
 
-#steps inside the physics world
-for i in range (1000):
+# Create a numpy vector filled with zeros
+backLegSensorValues = numpy.zeros(10000)
+#Create a numpy vector filled with zeros
+frontLegSensorValues = numpy.zeros(10000)
+
+# Steps inside the physics world
+for i in range(1000):
     p.stepSimulation()
-    #call times sleep function to slow down the simulation
+    # Retrieve sensor value inside the loop
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("back")
+    # Retrieve sensor value inside the loop
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("front")
+    # Slow down simulation
     t.sleep(1. / 100)
-    print (i) #tell us how long each iteration takes
 
+# Ensure 'data' directory exists
+if not os.path.exists("data"):
+    os.makedirs("data")
+
+# Save sensor data to disk
+numpy.save("data/back_leg_sensor.npy", backLegSensorValues)
+numpy.save("data/front_leg_sensor.npy", frontLegSensorValues)
+
+# Print the vector
+print(backLegSensorValues)
+print(frontLegSensorValues)
 
 p.disconnect()
-
