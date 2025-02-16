7c45f2d44a24a5d8c261922e12c2bf7cb1920941
megweyer <mweyer@uvm.edu>
2025-02-16 15:32:03 -0500

    end of assignment 5 - motors

--- analyze.py
+++ analyze.py
@@ -1,27 +1,30 @@
 import numpy
 import matplotlib.pyplot as plt
 
-#load in data
-backLegSensorValues = numpy.load("data/back_leg_sensor.npy")
-print (backLegSensorValues)
+# Load the saved motor command vectors
+targetAnglesBackLeg = numpy.load("data/target_angles_back.npy")
+targetAnglesFrontLeg = numpy.load("data/target_angles_front.npy")
 
-#plot the data
-#plt.subplot(1,2,1)
-plt.plot(backLegSensorValues, label = "Back Leg", linewidth = 3)
-#plt.title("Back Leg Sensor Values Over Time")
-
-
-#do the same for front leg
-#load in data
-frontLegSensorValues = numpy.load("data/front_leg_sensor.npy")
-print (frontLegSensorValues)
-
-#plot the data
-#plt.subplot(1,2,2)
-plt.plot(frontLegSensorValues, label = "Front Leg", linewidth = 0.5)
-#plt.title("Front Leg Sensor Values Over Time")
+# Generate x-values for plotting
+num_iterations = len(targetAnglesBackLeg)
+x_values = numpy.arange(num_iterations)
+# Plot both motor values
+plt.plot(x_values, targetAnglesFrontLeg, label="Front Leg", linewidth=2.5)
+plt.plot(x_values, targetAnglesBackLeg, label="Back Leg", linewidth=1)
+plt.xlabel("Time Step")
+plt.ylabel("Target Angle (radians)")
+plt.title("Front vs. Back Leg Motor Commands")
+plt.legend()
+# Show the plot
+plt.show()
 
 
-#show data
+
+#load in target angle data
+targetAngles = numpy.load("data/target_angles.npy")
+plt.plot(targetAngles, label = "Target Angles", linewidth = 0.5)
+plt.xlabel("Steps")
+plt.ylabel("Value in Radians")
+plt.title("Motor Commands")
 plt.legend()
 plt.show()
