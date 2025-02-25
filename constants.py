199e583ee6665d12397040f757b8bd4086d42066
megweyer <mweyer@uvm.edu>
2025-02-25 18:17:35 -0500

    end of refractoring

---
+++ constants.py
@@ -0,0 +1,20 @@
+import numpy
+#import all constants from other files
+
+#simulation parameters
+num_iterations = 1000
+max_force = 500
+sleep = 1/240
+
+#gravity constants
+xGrav = 0
+yGrav = 0
+zGrav = -9.8
+
+#motor parameters
+amplitude = numpy.pi / 4
+frequency = (2* numpy.pi)/num_iterations
+offset = numpy.pi
+
+
+
