edde83e164b61c201676e6d1ddaa4c07403af87e
megweyer <mweyer@uvm.edu>
2025-02-25 18:14:35 -0500

    end of refractoring

---
+++ simulation.py
@@ -0,0 +1,36 @@
+#import packages
+import pybullet as p
+import pybullet_data
+import time
+import constants as c
+from world import WORLD
+from robot import ROBOT
+
+class SIMULATION:
+    def __init__(self):
+        #connect to the physics engine and set up the simulation environment
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(c.xGrav, c.yGrav, c.zGrav)
+
+        #create instances
+        self.world = WORLD()
+        self.robot = ROBOT()
+
+    def Run(self):
+        for t in range(c.num_iterations):
+            p.stepSimulation()
+
+            #call robot method sense
+            self.robot.Sense(t)
+
+            #call the motors
+            self.robot.Act(t)
+
+            #slow down the simulation
+            time.sleep(c.sleep)
+
+    def __del__(self):
+        #disconnect from simulation
+        p.disconnect()
+
