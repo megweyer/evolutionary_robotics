36128983c010c15dba5f33610a11e2bd3b8e6d72
megweyer <mweyer@uvm.edu>
2025-03-03 18:26:45 -0500

    end of neurons - assignment 7

--- generate.py
+++ generate.py
@@ -1,20 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
+def Create_World ():
+    pyrosim.Start_SDF("world.sdf")  # stores info about the world
+    pyrosim.Send_Cube(name=f"Box_1", pos=[1, 1, 1], size=[1, 1, 1])  # sends cube for the world
 
-#create a function called create_world
-def Create_world ():
-    # where information about my world will be stored
-    pyrosim.Start_SDF("world.sdf")
-
-    # Send the cube with the current position and size
-    pyrosim.Send_Cube(name=f"Box_1", pos=[1, 1, 1], size=[1, 1, 1])
-
-    # close the sdf file
-    pyrosim.End()
-
-#create new robot with joints
-def Create_robot ():
-    pyrosim.Start_URDF("body.urdf")  # generate urdf file
+#create generate body function
+def Generate_Body ():
+    pyrosim.Start_URDF("body.urdf")  # generate urdf file of the robot body
     pyrosim.Send_Cube(name="torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])  # create torso
     pyrosim.Send_Joint(name="torso_back", parent="torso", child="back", type="revolute",
                        position=[1, 0, 1])  # create joint
@@ -22,4 +14,17 @@ def Create_robot ():
     pyrosim.Send_Joint(name="torso_front", parent="torso", child="front", type="revolute",
                        position=[2, 0, 1])  # create join
     pyrosim.Send_Cube(name="front", pos=[0.5, 0, -0.5], size=[1, 1, 1])  # front leg
-    pyrosim.End()
+    pyrosim.End() #ends simulation
+
+#create generate brain function using a neural network
+def Generate_Brain ():
+    pyrosim.Start_URDF("brain.nndf")  # generate nndf (neural network) file of the robot brain
+    pyrosim.Send_Sensor_Neuron(name=0, linkName="torso") #this line assigns a numeric value with each neuron - this one is for torso
+    pyrosim.Send_Sensor_Neuron(name=1, linkName="back")
+    pyrosim.Send_Sensor_Neuron(name=2, linkName="front")
+    pyrosim.Send_Motor_Neuron(name=3, jointName="torso_back")
+    pyrosim.Send_Motor_Neuron(name=4, jointName="torso_front")
+    pyrosim.End() #ends simulation
+
+Generate_Body()
+Generate_Brain()
