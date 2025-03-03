4c9429e3a0b0cf52462d54a75873524de2eefa9b
megweyer <mweyer@uvm.edu>
2025-03-03 18:28:30 -0500

    end of neurons - assignment 7

--- motor.py
+++ motor.py
@@ -6,32 +6,15 @@ import pyrosim.pyrosim as pyrosim
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_to_Act()
 
-    def Prepare_to_Act (self):
-        #create vector for motor
-        self.motorValues = np.zeros(c.num_iterations)
+    def Set_Value (self, robot, desiredAngle):
+        #Calculate the target motor command using a sine function.
+#        self.motorValues[t] = self.amplitude * np.sin(self.frequency * t + self.offset)
 
-        #set motor parameters
-        self.amplitude = c.amplitude
-        self.frequency = c.frequency
-        self.offset = c.offset
-
-        #modify frequency for torso front so it oscillates at half the frequency.
-        if self.jointName == "torso_front":
-            self.frequency = c.frequency / 2.0
-
-    def Set_Value (self, robot, t):
-        # Calculate the target motor command using a sine function.
-        self.motorValues[t] = self.amplitude * np.sin(self.frequency * t + self.offset)
-
-        #command the motor using PyBullet.
-        jointIndex = pyrosim.jointNamesToIndices[self.jointName]
-
-        #command the motor through pybullet
-        (p.setJointMotorControl2
+        jointIndex = pyrosim.jointNamesToIndices[self.jointName] #command the motor using PyBullet.
+        (p.setJointMotorControl2 #command the motor through pybullet
             (bodyIndex=robot.robotId,
             jointIndex=jointIndex,
             controlMode=p.POSITION_CONTROL,
-            targetPosition=self.motorValues[t],
+            targetPosition=desiredAngle,
             force=c.max_force))
